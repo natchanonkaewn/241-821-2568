ab5e30e62eba00e0c646333b0b2e5b8999b59aa1
natchanonkaewn <natchanon.kaewn@ku.th>
2025-12-30 11:09:28 +0700

    add js

---
+++ JS/main.js
@@ -0,0 +1,43 @@
+/*
+let firstname = "John";
+let age = 30;
+let height = 1.78;
+const PI = 3.14;
+
+console.log("firstname",firstname);
+console.log("age",age);
+console.log("height",height);
+
+let number1 = 10;
+let number2 = 3;
+let result1 = number1 + number2;
+console.log("ผลบวก =",result1);
+
+
+let number1 = 10;
+let number2 = 20;
+let condition = number1 < number2;
+console.log("condition",condition);
+
+
+let score = prompt;
+if (score >= 80) {
+    console.log("Grade A"); }
+else if (score >= 70) {
+    console.log("Grade B"); }
+else if (score >= 60) {
+    console.log("Grade C"); }
+else if (score >= 50) {
+    console.log("Grade D"); }
+else {
+    console.log("Grade F"); }
+*/
+
+let counter = 0;
+while (counter <= 4) {
+    counter += 1;
+    console.log("while:",counter);
+}
+for (let i = 1; i <= 5; i += 1) {
+    console.log("for:",i);
+}
