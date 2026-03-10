336f479e17b55ce70032272752fe658227040831
natchanonkaewn <natchanon.kaewn@ku.th>
2026-03-10 11:42:22 +0700

    add everything

--- Backend/index.js
+++ Backend/index.js
@@ -73,10 +73,40 @@ app.get('/users',async (req,res) => {
 //     res.json(users)
 // })
 
+const validateData = (userData) => {
+    let errors = []
+    if (!userData.firstname) {
+        errors.push("กรุณากรอกชื่อ")
+    }
+    if (!userData.lastname) {
+        errors.push("กรุณากรอกนามสกุล")
+    }
+    if (!userData.age) {
+        errors.push("กรุณากรอกอายุ")
+    }
+    if (!userData.gender) {
+        errors.push("กรุณาเลือกเพศ")
+    }
+    if (!userData.interests) {
+        errors.push("กรุณาเลือกความสนใจอย่างน้อย 1 อย่าง")
+    }
+    if (!userData.description) {
+        errors.push("กรุณากรอกคำอธิบายเกี่ยวกับตัวคุณ")
+    }
+    return errors
+}
+
 // path = POST /user
 app.post('/users',async (req,res) => {
     try {
         let user = req.body
+        const errors = validateData(user)
+        if (errors.length > 0) {
+            throw {
+                message: "กรอกข้อมูลไม่ครบถ้วน",
+                errors: errors
+            }
+        }
         const results = await conn.query('INSERT INTO users SET ?',user)
         console.log('results:',results)
         res.json({
@@ -84,10 +114,13 @@ app.post('/users',async (req,res) => {
             data: results[0]
         })
     } catch (error) {
-        console.error("Error creating user:",error)
+        const errorMessage = error.message || "Error creating user"
+        const errors = error.errors || []
+
+        console.error("Error creating user:",error.message)
         res.status(500).json({
-            message: "Error creating user",
-            error: error.message
+            message: error.message,
+            errors: errors
         })
     }
 })
--- Frontend/index.js
+++ Frontend/index.js
@@ -1,47 +1,96 @@
+const validateData = (userData) => {
+    let errors = []
+    if (!userData.firstname) {
+        errors.push("กรุณากรอกชื่อ")
+    }
+    if (!userData.lastname) {
+        errors.push("กรุณากรอกนามสกุล")
+    }
+    if (!userData.age) {
+        errors.push("กรุณากรอกอายุ")
+    }
+    if (!userData.gender) {
+        errors.push("กรุณาเลือกเพศ")
+    }
+    if (!userData.interests) {
+        errors.push("กรุณาเลือกความสนใจอย่างน้อย 1 อย่าง")
+    }
+    if (!userData.description) {
+        errors.push("กรุณากรอกคำอธิบายเกี่ยวกับตัวคุณ")
+    }
+    return errors
+}
+
 const submitData = async () => {
     let firstNameDOM = document.querySelector('input[name=firstname]')
     let lastNameDOM = document.querySelector('input[name=lastname]')
     let ageDOM = document.querySelector('input[name=age]')
-    let genderDOM = document.querySelector('input[name=gender]:checked')
-    let interestDOMs = document.querySelectorAll('input[name=interests]:checked')
+    let genderDOM = document.querySelector('input[name=gender]:checked') || {}
+    let interestDOMs = document.querySelectorAll('input[name=interests]:checked') || {}
     let descriptionDOM = document.querySelector('textarea[name=description]')
 
     let messageDOM = document.getElementById('message')
 
-    let userData
+    // let userData
 
     try {
         let interest = ''
-    for (let i = 0; i < interestDOMs.length; i++) {
-        interest += interestDOMs[i].value
-        if (i != interestDOMs.length - 1) {
-            interest += ','
+        for (let i = 0; i < interestDOMs.length; i++) {
+            interest += interestDOMs[i].value
+            if (i != interestDOMs.length - 1) {
+                interest += ','
+            }
         }
-    }
 
-    let userData = {
-        firstname: firstNameDOM.value,
-        lastname: lastNameDOM.value,
-        age: ageDOM.value,
-        gender: genderDOM.value,
-        description: descriptionDOM.value,
-        interests: interest
-    }
+        let userData = {
+            firstname: firstNameDOM.value,
+            lastname: lastNameDOM.value,
+            age: ageDOM.value,
+            gender: genderDOM.value,
+            description: descriptionDOM.value,
+            interests: interest
+        }
+
+        const errors = validateData(userData)
+        if (errors.length > 0) { // มี error > 0
+            throw {
+                message: "กรอกข้อมูลไม่ครบถ้วน",
+                errors: errors
+            }
+        }
 
         const response = await axios.post('http://localhost:8000/users',userData)
         console.log("response",response.data)
         messageDOM.innerText = "บันทึกข้อมูลสำเร็จ"
         messageDOM.className = 'message success'
 
-    } catch (error) {
+    } 
+    catch (error) {
+        console.log("error message",error.message)
+        console.log("error",error.errors)
+        
         if (error.response) {
             console.log("Error response:",error.response.data.message)
+            error.message = error.response.data.message
+            error.errors = error.response.data.errors
         }
-        messageDOM.innerText = "เกิดข้อผิดพลาด"
+
+        let htmlData = '<div>'
+        htmlData += `<div>${error.message}</div>`
+        htmlData += '<ul>'
+
+        for (let i = 0; i < error.errors.length; i++) {
+            htmlData += `<li>${error.errors[i]}</li>`
+        }
+
+        htmlData += '</ul>'
+        htmlData += '</div>'
+
+        messageDOM.innerHTML = htmlData
         messageDOM.className = 'message danger'
     }
 
-    console.log('submitData',userData)
+    // console.log('submitData',userData)
     
 }
 
